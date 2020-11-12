cee5aa4f548db57b6524755b9211df5c3315186b
rajarshi07 <rajarshighoshdastidar@gmail.com>
2020-11-12 11:34:53 +0530

    Initial Commit

---
+++ firstapp/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import events, registration, date_revenue, society_leads, state_connection, coupons
+
+# Register your models here.
+
+admin.site.register(events)
+admin.site.register(date_revenue)
+admin.site.register(society_leads)
+admin.site.register(state_connection)
+admin.site.register(coupons)
+
+class RegistrationAdmin(admin.ModelAdmin):
+    class Meta:
+        model = registration
+        fields = "__all__"
+    list_display = ('name','event','email','participant_id',"coupon","transaction_id","timestamp")
+    search_fields =  ('name', 'event', 'email', 'participant_id',"coupon","transaction_id","timestamp")
+
+admin.site.register(registration,RegistrationAdmin)
---
+++ firstapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms 
+from .models import events
+  
+  
+# creating a form 
+class eventsForm(forms.ModelForm): 
+  
+    # create meta class 
+    class Meta: 
+        # specify model to be used 
+        model = events
+  
+        # specify fields to be used 
+        fields = ["name","select_state","cluster","summary","picture","rules","cost","start_day","group_event"] 
